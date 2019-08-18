15594634259fb201d71ff5ffce653fdec4362186
Doggoduck <34210285+kipkat@users.noreply.github.com>
2019-08-18 22:40:58 +0300

    Change some details and the redirection method

--- paste.php
+++ paste.php
@@ -1,5 +1,4 @@
 <?php
-date_default_timezone_set("America/New_York");
 $fname= "./paste/".date("ydhisms").'.txt';
 $myfile = fopen($fname, "w") or die("Unable to create/open file!");
 $txt = $_POST["xcode"];
@@ -9,16 +8,14 @@ $files = glob("./paste/"."*");
 $now   = time();
 
 foreach ($files as $file) {
-if (is_file($file)) {
-  if ($now - filemtime($file) >= 60 * 60 * 5) { // 2 hours
-      if ($file != './paste/index.html') { // 2 hours
-        unlink ($file);
-      }
+  if (is_file($file)) {
+    if ($now - filemtime($file) >= 60 * 60 * 5) {
+        if ($file != './index.html') {
+          unlink ($file);
+        }
+    }
   }
 }
-}
+
+Header("Location: /..");
 ?>
-<script>
-var redir = "<?php echo $fname ?>";
-window.location.replace(redir);
-</script>

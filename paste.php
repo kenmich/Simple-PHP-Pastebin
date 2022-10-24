1d2ee3149cac42e6f33d3b0a32491ba724dd0646
kenmich <104056274+kenmich@users.noreply.github.com>
2022-10-24 13:08:55 +0200

    Update paste.php

--- paste.php
+++ paste.php
@@ -1,15 +1,15 @@
 <?php
-$fname= "./paste/".date("ydhisms").'.txt';
+$fname= "./p/".date("ms").'.txt';
 $myfile = fopen($fname, "w") or die("Unable to create/open file!");
 $txt = $_POST["xcode"];
 fwrite($myfile, $txt);
 fclose($myfile);
-$files = glob("./paste/"."*");
+$files = glob("./p/"."*");
 $now   = time();
 
 foreach ($files as $file) {
   if (is_file($file)) {
-    if ($now - filemtime($file) >= 60 * 60 * 5) {
+    if ($now - filemtime($file) >= 60 * 60 * 1) {
         if ($file != './index.html') {
           unlink ($file);
         }

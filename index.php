f527bb0f24f424be227cced438179490c27a8c29
kenmich <104056274+kenmich@users.noreply.github.com>
2022-10-24 13:08:34 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,41 +4,41 @@
 		<h1>Simple Pastebin<h1>
 		<h3>made by kipkat</h3>
 		<h3><?php
-			echo "Hosting ".(iterator_count(new FilesystemIterator("./paste/", FilesystemIterator::SKIP_DOTS))-1)." pastes. Every paste expires in 5 hours.";
+			echo "Hosting ".(iterator_count(new FilesystemIterator("./p/", FilesystemIterator::SKIP_DOTS))-1)." pastes. Every paste expires in 1 hour.";
 		?></h3>
 	</center>
 	<style>	
 		h1 {
-			color: red;
+			color: black;
 		}
 		h3 {
-			color: blue;
+			color: green;
 			margin-top: -20px;
 		}
 		body {
-			background-color: #000;
+			background-color: #ffffff;
 		}
 		textarea {
-			background-color: #111;
-			border: 3px solid blue;
-			color: #fff;
+			background-color: #ffffff;
+			border: 2px solid black;
+			color: #000;
 		}
 		input {
 			padding: 3px;
 			font-size: 40px;
-			background-color: #222;
+			background-color: #ffffff;
 			border: none;
-			color: #fff;
+			color: #000;
 		}
 		input:hover {
-			border: 2px solid red;
+			border: 1px solid black;
 		}
 	</style>
 </head>
 <body>
 <center>
 	<p><textarea style="margin: 0px; height: 60%; width: 100%;" form="usrform" id="xcode" name="xcode" placeholder="code here"></textarea></p>
-	<form id="usrform" action="./paste.php" method="post" enctype="multipart/form-data"><input type="submit" value='Upload to server'/></form>
+	<form id="usrform" action="./paste.php" method="post" enctype="multipart/form-data"><input type="submit" value='Upload'/></form>
 </center>
 </body>
 </html>
